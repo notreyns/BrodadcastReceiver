6d2ba097635dde50674d6e78647430537def94b0
Reina Khasanova <reina.khasanova@iaau.edu.kg>
2022-04-26 16:32:01 +0600

    imitation of loading, creating receiver in specific activity using anonymous class

--- app/src/main/java/com/neobis/receiver/MainActivity.kt
+++ app/src/main/java/com/neobis/receiver/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.neobis.receiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private val receiver = MyReceiver()
+    private lateinit var progressBar: ProgressBar
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "loaded") {
+                val percent = intent.getIntExtra("percent", 0)
+                Toast.makeText(context, "Loaded $percent", Toast.LENGTH_SHORT).show()
+                progressBar.setProgress(percent)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        progressBar = findViewById(R.id.progressBar)
         var counter = 0
         findViewById<Button>(R.id.button).setOnClickListener {
             val intent = Intent(MyReceiver.MY_OWN_ACTION).putExtra(MyReceiver.counter, ++counter)
@@ -21,8 +34,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         val intentFilter = IntentFilter().apply {
-            addAction(Intent.ACTION_BATTERY_LOW)
-            addAction(MyReceiver.MY_OWN_ACTION)
+            addAction("loaded")
+        }
+        Intent(this, MyService::class.java).apply {
+            startService(this)
         }
         registerReceiver(receiver, intentFilter)
     }
